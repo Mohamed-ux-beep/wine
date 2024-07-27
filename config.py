a1d55a73793630d14ace26014b8d3c82b11aef72
mohamed <mohamed.abokahf@student.hs-anhalt.de>
2024-07-27 08:51:02 +0200

    new update

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import os
 ######## getting the directory of the file ############
 #######################################################
 
-home = Path().resolve().parent.as_posix() + '/data'
-data = os.listdir(home)[0]
+home = Path().resolve().as_posix()
+data = os.listdir(home)[5]
 data_dir = home + '/' + data
+
 
